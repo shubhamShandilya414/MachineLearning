bfcdad388577061d46f8837906d82320372662da
shubhamShandilya414 <sshandilya414@gmail.com>
2026-02-27 01:45:37 +0530

    Update OrderServiceApplication.java

--- src/main/java/com/javatechie/aws/cicd/example/OrderServiceApplication.java
+++ src/main/java/com/javatechie/aws/cicd/example/OrderServiceApplication.java
@@ -24,15 +24,7 @@ public List<Order> fetchOrders() {
     return orderDao.getOrders().stream()
             .filter(order -> order.getPrice() > 100)
             .sorted(Comparator.comparing(Order::getPrice))
-            .collect(Collectors.toList());
-
-    
-
-    
-    
-    
-
-    
+            .collect(Collectors.toList()); 
     
 }
     public static void main(String[] args) {
