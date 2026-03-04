ab8d276188d9f8995837360b41ec9acd0b5454a7
shubhamShandilya414 <sshandilya414@gmail.com>
2026-03-05 04:41:46 +0530

    chore: update OrderServiceApplication.java

--- src/main/java/com/javatechie/aws/cicd/example/OrderServiceApplication.java
+++ src/main/java/com/javatechie/aws/cicd/example/OrderServiceApplication.java
@@ -1,4 +1,4 @@
-package com.javatechie/aws/cicd/example;
+package com.javatechie.aws.cicd.example;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
