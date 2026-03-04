536afa7f7403c13c12f89bb97c4cddf5bf1ef6b4
shubhamShandilya414 <sshandilya414@gmail.com>
2026-03-05 04:45:48 +0530

    chore: update OrderServiceApplicationTests.java

--- src/test/java/com/javatechie/aws/cicd/example/OrderServiceApplicationTests.java
+++ src/test/java/com/javatechie/aws/cicd/example/OrderServiceApplicationTests.java
@@ -1,13 +1,22 @@
-package com.javatechie.aws.cicd.example;
+package com/javatechie/aws/cicd/example;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class OrderServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private OrderServiceApplication orderServiceApplication;
 
+    @Test
+    void testFetchOrders() {
+        List<Order> orders = orderServiceApplication.fetchOrders();
+        assertTrue(orders.stream().allMatch(order -> order.getId() > 100)); // NEW
+    }
 }
