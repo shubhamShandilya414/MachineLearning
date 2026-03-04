25f502d7ba6daf5a4bf7724453d2b08e04f53c6b
shubhamShandilya414 <sshandilya414@gmail.com>
2026-03-05 03:38:23 +0530

    chore: update OrderServiceApplicationTests.java

--- src/test/java/com/javatechie/aws/cicd/example/OrderServiceApplicationTests.java
+++ src/test/java/com/javatechie/aws/cicd/example/OrderServiceApplicationTests.java
@@ -1,13 +1,27 @@
-package com.javatechie.aws.cicd.example;
+
+package com.javatechie/aws/cicd/example;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
 
-@SpringBootTest
-class OrderServiceApplicationTests {
+import java.util.List;
 
-	@Test
-	void contextLoads() {
-	}
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class OrderServiceApplicationTests {
+
+    @Autowired
+    private TestRestTemplate testRestTemplate;
+
+    @Test
+    void testFetchOrders() {
+        ResponseEntity<List<Order>> response = testRestTemplate.getForEntity("/orders", List.class);
+        List<Order> orders = response.getBody();
+        // NEW: verify that all orders have id > 100
+        orders.forEach(order -> assertEquals(true, order.getId() > 100));
+    }
 }
