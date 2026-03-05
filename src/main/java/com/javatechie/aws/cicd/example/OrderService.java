0f10f2d8e4cf9f8b355a95d5dcb206420c62ffd0
shubhamShandilya414 <sshandilya414@gmail.com>
2026-03-05 14:23:57 +0530

    chore: update OrderService.java

---
+++ src/main/java/com/javatechie/aws/cicd/example/OrderService.java
@@ -0,0 +1,42 @@
+package com.javatechie.aws.cicd.example;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@Service
+public class OrderService {
+
+    private final OrderDao orderDao;
+
+    @Autowired
+    public OrderService(OrderDao orderDao) {
+        this.orderDao = orderDao;
+    }
+
+    public List<Order> getOrders() {
+        return orderDao.getOrders().stream()
+                .sorted(Comparator.comparingLong(Order::getPrice))
+                .collect(Collectors.toList());
+    }
+
+    // NEW
+    public Order getOrderById(int id) {
+        return orderDao.getOrders().stream()
+                .filter(order -> order.getId() == id)
+                .findFirst()
+                .orElse(null);
+    }
+
+    // NEW
+    public List<Order> getOrdersWithFiltering(int minPrice, int pageNumber, int pageSize) {
+        return orderDao.getOrders().stream()
+                .filter(order -> order.getPrice() >= minPrice)
+                .skip((pageNumber - 1) * pageSize)
+                .limit(pageSize)
+                .collect(Collectors.toList());
+    }
+}
